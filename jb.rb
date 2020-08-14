3f2ff1e10178af3058986777131e68587c4f7f34
ASalimov <salimov.artem@gmail.com>
2020-08-14 16:31:49 +0300

    Update jb.rb

--- jb.rb
+++ jb.rb
@@ -1,8 +1,8 @@
 class Jb < Formula
   desc ""
   homepage ""
-  url "https://github.com/ASalimov/jb/archive/v1.1.tar.gz"
-  sha256 "1289595d80b9a9fed823f49e3d509cc3737df304b1ec666499ecf9e4f066a18f"
+  url "https://github.com/ASalimov/jb/archive/v1.2.tar.gz"
+  sha256 "d43987a409b26390c231bbd2f0f9e9d8d5444e76e8e93e1614ef786f518b6cc8"
   license ""
 
   depends_on "go" => :build
