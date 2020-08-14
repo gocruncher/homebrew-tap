28b8d94bbd51e3f6f2135affb34b70a30b221bdd
ASalimov <salimov.artem@gmail.com>
2020-08-14 17:02:33 +0300

    Update jb.rb

--- jb.rb
+++ jb.rb
@@ -1,14 +1,14 @@
 class Jb < Formula
   desc ""
   homepage ""
-  url "https://github.com/ASalimov/jbuilder/archive/v1.3.tar.gz"
-  sha256 "ea14daece882e56baff6da5bc1e06d39f7ace5250c622a8ebb6145edc043e0c2"
+  url "https://github.com/ASalimov/jbuilder/archive/v1.4.tar.gz"
+  sha256 "0ad84079b093054ba29ed9371db8ec4ea77cc9f35b7b982601628db96772b03c"
   license ""
 
   depends_on "go" => :build
   def install
     system "./gobuild.sh"
-    bin.install ".gobuild/bin/jb" => "jb"
+    bin.install ".gobuild/bin/jbuilder" => "jb"
   end
 
   test do
