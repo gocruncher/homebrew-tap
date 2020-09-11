b96834b27ffb46ca0ffe702ad042421800ede944
gocruncher <salimov.artem@gmail.com>
2020-09-11 20:59:54 +0300

    Update jb.rb

--- jb.rb
+++ jb.rb
@@ -1,8 +1,8 @@
 class Jb < Formula
   desc ""
   homepage ""
-  url "https://github.com/gocruncher/jbuilder/archive/v1.12.tar.gz"
-  sha256 "7f34ee9ee671fc9bad237b2049bb259f35bdf355ab61b542fb90ec9fcf0ea966"
+  url "https://github.com/gocruncher/jbuilder/archive/v1.0.0.tar.gz"
+  sha256 "02a1e037320848354d535b181a366e2e07c6385d84630862ac30f208659fa6c4"
   license ""
 
   depends_on "go" => :build
