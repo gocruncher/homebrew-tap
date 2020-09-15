d17dfb9cf817260affac781e69264a59459625dd
gocruncher <salimov.artem@gmail.com>
2020-09-15 15:11:41 +0300

    Update jj.rb

--- jj.rb
+++ jj.rb
@@ -1,14 +1,14 @@
 class Jj < Formula
   desc ""
   homepage ""
-  url "https://github.com/gocruncher/jenkins-job-ctl/archive/v1.1.2.tar.gz"
+  url "https://github.com/gocruncher/jenkins-job-cli/archive/v1.1.2.tar.gz"
   sha256 "60cfd0f7d100edecb5428f98a77e5492438ea40377bfd20852fa5576ea7f184a"
   license ""
 
   depends_on "go" => :build
   def install
     system "./gobuild.sh"
-    bin.install ".gobuild/bin/jenkins-job-ctl" => "jj"
+    bin.install ".gobuild/bin/jenkins-job-cli" => "jj"
   end
 
   test do
