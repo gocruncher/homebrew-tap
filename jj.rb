5cb5a720b95f18caa44dbf18544ac43c3d289cf4
gocruncher <salimov.artem@gmail.com>
2020-09-15 14:54:39 +0300

    Update and rename jb.rb to jj.rb

--- jj.rb
+++ jj.rb
@@ -1,17 +1,17 @@
-class Jb < Formula
+class Jj < Formula
   desc ""
   homepage ""
-  url "https://github.com/gocruncher/jbuilder/archive/v1.0.1.tar.gz"
+  url "https://github.com/gocruncher/jenkins-job-ctl/archive/v1.1.0.tar.gz"
   sha256 "355f0646dc444e7ca13baa694152c29a92187eb8620a28d7d0d6760e394baac7"
   license ""
 
   depends_on "go" => :build
   def install
     system "./gobuild.sh"
-    bin.install ".gobuild/bin/jbuilder" => "jb"
+    bin.install ".gobuild/bin/jenkins-job-ctl" => "jj"
   end
 
   test do
-    system "#{bin}/jb", "--help"
+    system "#{bin}/jj", "--help"
   end
 end
